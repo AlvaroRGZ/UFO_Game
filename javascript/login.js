01c1699be657a74285de1564abc211a69febe75b
Alvaro Rodriguez Gomez <alu0101362953@ull.edu.es>
2023-11-17 16:16:04 +0100

    Fixed records table

---
+++ javascript/login.js
@@ -0,0 +1,21 @@
+
+function dologin(){ 
+  let url = "http://wd.etsisi.upm.es:10000/users/login?";   
+  url += "username=" + document.getElementById('username').value + "&";
+  url += "password=" + document.getElementById('password').value;
+
+  fetch(url, { method: 'GET' })
+    .then(response => {
+      if (response.status === 200) {
+        let jwtToken = response.headers.get('authorization');
+        document.getElementById('resultado').innerHTML = jwtToken;
+      } else {
+        alert("There was an ERROR with the URL");
+      }
+    })
+    .catch(err => console.error(err));
+}
+
+window.onload = function(){
+  document.getElementById('info').onclick = dologin;
+}
---
+++ javascript/records.js
@@ -0,0 +1,48 @@
+
+function responseProcess(http_request) {
+  if(http_request.response.status == 200) {
+    jwtToken = http_request.getAllResponseHeaders().match(/authorization:*/i)[0];
+    document.getElementById('resultado').innerHTML = jwtToken;
+  } else {
+    alert("There was an ERROR with the URL");
+  }
+} 
+
+function loadRecords(){ 
+  let table = document.getElementById('records_table'); 
+
+  fetch('http://wd.etsisi.upm.es:10000/records', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }).then(function(response) {
+      if (response.ok)
+        return response.json();
+    })
+    .then(function(data) {
+      let html = '';
+      for (let record of data) {
+        html += '<tr>';
+        for (let prop in record) {
+          if (prop === 'recordDate') {
+            let date = new Date(record[prop]);
+            html += `<td>${date.toLocaleDateString()} ${date.toLocaleTimeString()}</td>`;
+          } else {
+            html += `<td>${record[prop]}</td>`;
+          }
+        }
+        html += '</tr>';
+      }
+      table.innerHTML += html;
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+}
+
+window.onload = function(){
+  loadRecords();
+}
+
+
